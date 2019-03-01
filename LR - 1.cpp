1946e2905df4b2041732045549c2866f2e8cc66e
Tim <40692433+Osipchik@users.noreply.github.com>
2019-03-01 13:49:42 +0300

    Update LR - 1.cpp

--- LR - 1.cpp
+++ LR - 1.cpp
@@ -1,6 +1,4 @@
-
 #include <iomanip>
-#include <algorithm>
 #include <stdio.h>
 
 using namespace std;
@@ -10,30 +8,24 @@ void show(int a, int b, int indent);
 
 int main()
 {
-  int numA, numB;
-  scanf_s("%u", &numA);
-  scanf_s("%u", &numB);
+	int numA, numB;
+	printf("input numA: ");
+	scanf_s("%u", &numA);
+	printf("input numB: ");
+	scanf_s("%u", &numB);
 
-  system("cls");
-  
-  if (numA < numB)
-  {
-	  numA = numA ^ numB;
-	  numB = numA ^ numB;
-	  numA = numA ^ numB;
-  }
+	system("cls");
 
-  int indent;
-
-  indent = Get_indent(numA * numB);
-  show(numA, numB, indent);
-  printf("\n%*u ", indent, numA * numB);
+	int indent = Get_indent(numA * numB);
+	
+	show(numA, numB, indent);
+	printf("\n%*u ", indent, numA * numB);
 	return 0;
 }
 
-int Get_indent(int rezult)
-{
-	int indent = 1;
+int Get_indent(int rezult)                 //Функция для посчета отступа
+{                                         
+	int indent = 2;
 	do
 	{
 		indent++;
@@ -50,15 +42,13 @@ void show(int numA, int numB, int indent)
 	printf("\n%*u ", indent, numB);
 	printf("\n ---------");
 
-	int i = 1;
 	if (numB / 10)
 	{
 		do
 		{
-			if (numB / 10 == 0) printf("\n%*u", indent - i, numA* (numB % 10));
-			else printf("\n+%*u", indent - i, numA * (numB % 10));
+			if (numB / 10 == 0) printf("\n%*u", indent, numA* (numB % 10));
+			else printf("\n+%*u", --indent, numA * (numB % 10));
 			numB /= 10;
-			i++;
 		} while (numB);
 		printf("\n ---------");
 	}
